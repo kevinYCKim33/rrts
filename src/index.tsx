a01506e9f9dc8802e690ceab1c0223e2c1d4b4d8
Kevin <kevinyckim33@gmail.com>
2021-02-26 09:54:49 -0500

    Finish 266 of 279: Generic Dispatch Function

--- src/index.tsx
+++ src/index.tsx
@@ -1,36 +1,23 @@
 // https://github.com/FEDCAP/single-stop-frontend-code-challenge-SUBMISSIONS/pull/6/files
+// yarn add redux react-redux axios redux-thunk
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { App } from "./components/App";
+import { reducers } from "./reducers";
 
-interface AppProps {
-  color?: string; // ? for optional
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
 // Functional Component Syntax: huge for the future
 // const App = (props: AppProps): JSX.Element => {
 //   return <div>{props.color}</div>;
 // };
 
-class App extends React.Component<AppProps> {
-  state = { counter: 1 }; // TLDR: don't do constructors in Typescript + React, confusing enough;
-
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App color={"red"} />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
